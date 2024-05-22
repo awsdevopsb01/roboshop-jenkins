c220f44500e321a182e9fe5c550298a80a55ffb8
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-05-22 20:49:47 +0530

    code quality - sonarqube

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -17,22 +17,18 @@ def call() {
                     sh ' sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.43.16:9000'
                 }
             }
-        }
-        stages {
+
             stage('Unit Test Cases') {
                 steps {
                     sh 'echo Unit Test Cases'
                 }
             }
-        }
-        stages {
             stage('Checkmarx SAST Scan') {
                 steps {
                     sh 'echo Unit Test Cases'
                 }
             }
-        }
-        stages {
+
             stage('Checkmarx SCA Scan') {
                 steps {
                     sh 'echo Unit Test Cases'
