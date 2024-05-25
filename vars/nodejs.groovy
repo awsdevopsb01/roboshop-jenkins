4873428b48dc8473c96f8c139df4bea7fb18ad49
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-05-25 20:26:57 +0530

    remove sonarqube

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,11 +14,10 @@ def call() {
         stages {
             stage('Code Quality') {
                 steps {
-                    sh 'Code Quality'
+                    sh 'echo Code Quality'
 //                    sh ' sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.37.59:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                     }
             }
-
             stage('Unit Test Cases') {
                 steps {
                     npm test
@@ -26,13 +25,13 @@ def call() {
             }
             stage('Checkmarx SAST Scan') {
                 steps {
-                    sh 'echo Unit Test Cases'
+                    sh 'echo SAST Scan'
                 }
             }
 
             stage('Checkmarx SCA Scan') {
                 steps {
-                    sh 'echo Unit Test Cases'
+                    sh 'echo SCA Scan'
                 }
             }
         }
