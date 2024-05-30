ea19bcdf4abc3d47bf8f3f64c2cfafaa9b6f9ca5
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-05-30 13:47:07 +0530

    include schema directory in the artifact

--- vars/python.groovy
+++ vars/python.groovy
@@ -46,7 +46,7 @@ def call() {
                 }
                 steps {
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION ${schema_dir}'
                     sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.34.184:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
